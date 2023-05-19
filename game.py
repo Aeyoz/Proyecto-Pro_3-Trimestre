78d9245ac81d730c94edab889526483af36745b2
adrianhrb <adrian.herrera.br@gmail.com>
2023-05-19 16:56:53 +0100

    Se avanza proyecto

--- game.py
+++ game.py
@@ -6,12 +6,13 @@ import roles
 class Game:
     def __init__(self, number_of_players: int):
         self.deck = cards.Deck()
-        self.players = []
-        for i in range(number_of_players):
-            player = f"Player{i + 1}"
-            self.players.append(roles.Player(player))
+        self.players = [
+            roles.Player(f"Player{i + 1}") for i in range(number_of_players)
+        ]
         self.dealer = roles.Dealer()
+        self.dealer.players = self.players
 
     def start_game(self):
         for player in self.players:
             player.cards = [self.dealer.give_players_cards()]
+        common_cards = self.dealer.show_comunity_cards()
