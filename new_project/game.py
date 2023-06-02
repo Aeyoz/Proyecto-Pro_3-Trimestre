8bca8701f5ce5af0558ac12418358a76612cebfd
adrianhrb <adrian.herrera.br@gmail.com>
2023-06-02 17:15:50 +0100

    Añadidos cambios

--- new_project/game.py
+++ new_project/game.py
@@ -14,15 +14,12 @@ class Game:
         ]
 
     def start_game(self):
-        self.dealer = roles.Dealer()
-        self.deck = self.dealer.deck = cards.Deck()
-        self.dealer.deck.shuffle_deck()
-        self.dealer.players = self.players
         self.community_cards = self.dealer.display_community_cards()
-        print(" |  ".join(f"{card.value}{card.suit}" for card in self.community_cards))
+
+    def get_winner(self):
+        # print(" |  ".join(f"{card.value}{card.suit}" for card in self.community_cards))
         best_comb = ()
         for player in self.players:
-            player.get_cards(self.dealer)
             player.common_cards = self.community_cards
             player.best_combination = player.get_best_combination() + (player.name,)
             if not best_comb or player.best_combination[0] > best_comb[0]:
@@ -39,11 +36,28 @@ class Game:
                     best_comb += (player.name, "Tie")
 
             punctuation, *player_cards, player_name = (80, 4, "player1")
-            print(player.name)
-            print(" |  ".join(f"{card.value}{card.suit}" for card in player.cards))
-            print(player.best_combination)
-            print("\n")
+            # print(player.name)
+            # print(" |  ".join(f"{card.value}{card.suit}" for card in player.cards))
+            # print(player.best_combination)
+            # print("\n")
         return best_comb[-1]
+
+        def get_winner(self, p1, p2):
+            tie = False
+            player1_punctuation, *player1_cards = p1
+            player2_punctuation, *player2_cards = p2
+            if (
+                player1_punctuation > player2_punctuation
+                or player2_punctuation > player1_punctuation
+            ):
+                return int(player1_punctuation > player2_punctuation)
+            for card1, card2 in zip(p1, p2):
+                if card1 == card2:
+                    tie = True
+                if card1 > card2:
+                    "algo"
+                if card2 > card1:
+                    "algo"
 
 
 new_game = Game(7)
